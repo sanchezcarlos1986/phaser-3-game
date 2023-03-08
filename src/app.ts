c38a58da7823cb28f5e36fd4f353b1e9723c59e9
Carlos Sanchez <carlossanchez.globant@latam.com>
2023-03-08 18:14:52 -0300

    add physics

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import { Preload } from "./scenes/Preload";
-import { IntroCapcom } from "./scenes/IntroCapcom";
-import { IntroStory } from "./scenes/IntroStory";
+// import { IntroCapcom } from "./scenes/IntroCapcom";
+// import { IntroStory } from "./scenes/IntroStory";
 import { Level } from "./scenes/Level";
 
 const config: Phaser.Types.Core.GameConfig = {
   title: "Demo Game",
   pixelArt: true,
   scene: [Preload, Level],
-  backgroundColor: "#000",
+  backgroundColor: "#ccc",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 800,
+      },
+      debug: true,
+      debugShowBody: true,
+      debugShowStaticBody: true,
+    },
+  },
   scale: {
     mode: Phaser.Scale.FIT,
     parent: "game-container",
--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -1,11 +1,12 @@
 import { Scene } from "phaser";
 
 export class Level extends Scene {
-  cubix: Phaser.GameObjects.Image;
   leftKey: Phaser.Input.Keyboard.Key;
   rightKey: Phaser.Input.Keyboard.Key;
   upKey: Phaser.Input.Keyboard.Key;
   downKey: Phaser.Input.Keyboard.Key;
+  cubix: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+  tomato: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   constructor() {
     super({
       key: "LevelScene",
@@ -15,19 +16,51 @@ export class Level extends Scene {
   preload() {
     this.load.setPath("assets");
     this.load.image("cubix", "images/cubix.png");
+
+    this.load.atlas(
+      "tomato",
+      "sprites/tomato.png",
+      "sprites/tomato_atlas.json"
+    );
+    this.load.animation("tomatoAnim", "sprites/tomato_anim.json");
   }
 
   create(): void {
-    this.add
-      .text(400, 400, "TEXTO DE MEGA MAN", {
-        color: "white",
-        align: "center",
-        fontSize: "20px",
-        fontFamily: '"Press Start 2P"',
-      })
-      .setOrigin(0.5);
+    //sprites
+    this.tomato = this.physics.add
+      .sprite(300, 300, "tomato")
+      .setScale(4)
+      .refreshBody();
+    this.tomato.anims.play("tomato_walk");
 
-    this.cubix = this.add.image(100, 100, "cubix").setOrigin(0.5);
+    this.cubix = this.physics.add
+      .image(100, 100, "cubix")
+      .setOrigin(0.5)
+      .refreshBody();
+
+    this.cubix.body.setCollideWorldBounds(true);
+    this.tomato.body.setCollideWorldBounds(true);
+
+    // this.add.tween({
+    //   targets: [this.cubix],
+    //   ease: "Bounce",
+    //   y: 600,
+    // });
+
+    this.physics.add.collider(this.cubix, this.tomato);
+
+    this.cubix.body.setBounceY(0.5);
+
+    // text
+    // this.add
+    //   .text(400, 400, "TEXTO DE MEGA MAN", {
+    //     color: "white",
+    //     align: "center",
+    //     fontSize: "20px",
+    //     fontFamily: '"Press Start 2P"',
+    //   })
+    //   .setOrigin(0.5);
+
     const keyCodes = Phaser.Input.Keyboard.KeyCodes;
 
     this.leftKey = this.input.keyboard.addKey(keyCodes.LEFT);
@@ -35,32 +68,28 @@ export class Level extends Scene {
     this.upKey = this.input.keyboard.addKey(keyCodes.UP);
     this.downKey = this.input.keyboard.addKey(keyCodes.DOWN);
 
-    this.leftKey.on("down", () => {
-      console.log("left");
-    });
-
+    this.leftKey.on("down", () => console.log("left"));
     this.rightKey.on("down", () => {
-      console.log("right");
+      this.cubix.body.setAcceleration(800, 0);
     });
-
-    this.upKey.on("down", () => {
-      console.log("up");
-    });
-
-    this.downKey.on("down", () => {
-      console.log("down");
-    });
+    this.upKey.on("down", () => console.log("up"));
+    this.downKey.on("down", () => console.log("down"));
   }
 
   update() {
-    if (this.leftKey.isDown) {
-      this.cubix.setScale(-1, 1);
-      this.cubix.x--;
-    }
-
-    if (this.rightKey.isDown) {
-      this.cubix.setScale(1, 1);
-      this.cubix.x++;
-    }
+    // if (this.leftKey.isDown) {
+    //   this.cubix.setScale(-1, 1);
+    //   this.cubix.x = this.cubix.x - 8;
+    // }
+    // if (this.rightKey.isDown) {
+    //   this.cubix.setScale(1);
+    //   this.cubix.x = this.cubix.x + 8;
+    // }
+    // if (this.upKey.isDown) {
+    //   this.cubix.y = this.cubix.y - 8;
+    // }
+    // if (this.downKey.isDown) {
+    //   this.cubix.y = this.cubix.y + 8;
+    // }
   }
 }
